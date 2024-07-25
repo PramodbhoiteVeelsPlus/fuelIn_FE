9fa9e8c88760d9f4a104fee4b614077b38c8faed
pramod bhoite <pramod.bhoite@veels.plus>
2024-07-25 15:09:30 +0530

    changes added by PRAMOD

--- src/app/_metronic/partials/content/widgets/tables/tables-widget10/tables-widget10.component.ts
+++ src/app/_metronic/partials/content/widgets/tables/tables-widget10/tables-widget10.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
 
 @Component({
   selector: 'app-tables-widget10',
   templateUrl: './tables-widget10.component.html',
 })
 export class TablesWidget10Component implements OnInit {
+  closingDate = moment(new Date()).format("YYYY-MM-DD")
+  openingDate = moment(new Date()).format("YYYY-MM-01")
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
