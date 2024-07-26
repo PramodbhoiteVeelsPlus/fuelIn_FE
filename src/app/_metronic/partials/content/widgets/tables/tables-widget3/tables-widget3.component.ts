3a4293332295d2be09b463e2f2b838718443bb80
pramod bhoite <pramod.bhoite@veels.plus>
2024-07-26 17:29:42 +0530

    changes added by PRAMOD

--- src/app/_metronic/partials/content/widgets/tables/tables-widget3/tables-widget3.component.ts
+++ src/app/_metronic/partials/content/widgets/tables/tables-widget3/tables-widget3.component.ts
@@ -5,7 +5,64 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tables-widget3.component.html',
 })
 export class TablesWidget3Component implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor() { }
+  staffArray = [
+    {
+      accessGroupId:12,
+      addressId:873,
+      city:"PUNE",
+      dealerMapStatus:"MAPPED",
+      designation:"MANAGER",
+      firstName:"REALMEOW",
+      fuelDealerStaffId:12,
+      lastName:"",
+      personId:1215,
+      phone1:"9765592000",
+      salary:"NOT ADDED",
+      userId:"725",
+    },
+    {
+      accessGroupId:14,
+      addressId:873,
+      city:"PUNE",
+      dealerMapStatus:"MAPPED",
+      designation:"MANAGER",
+      firstName:"REALMEOW",
+      fuelDealerStaffId:12,
+      lastName:"",
+      personId:1215,
+      phone1:"9765592000",
+      salary:"10000",
+      userId:"725",
+    },
+    {
+      accessGroupId:13,
+      addressId:873,
+      city:"PUNE",
+      dealerMapStatus:"MAPPED",
+      designation:"OPERATOR",
+      firstName:"OPERATOR1",
+      fuelDealerStaffId:12,
+      lastName:"",
+      personId:1215,
+      phone1:"9765592000",
+      salary:"8000",
+      userId:"725",
+    },
+    {
+      accessGroupId:13,
+      addressId:873,
+      city:"PUNE",
+      dealerMapStatus:"MAPPED",
+      designation:"OPERATOR",
+      firstName:"OPERATOR2",
+      fuelDealerStaffId:12,
+      lastName:"",
+      personId:1215,
+      phone1:"9765592000",
+      salary:"5000",
+      userId:"725",
+    }
+  ];
+  ngOnInit(): void { }
 }
