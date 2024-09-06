89af787eb9ad07f28f5acab499f654e8ecb6abdc
pramod bhoite <pramod.bhoite@veels.plus>
2024-09-06 10:48:17 +0530

    changes added by PRAMOD

---
+++ src/app/pages/dashboard/admin dashboard/dashboard4/dashboard4.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard4',
+  templateUrl: './dashboard4.component.html',
+  styleUrl: './dashboard4.component.scss'
+})
+export class Dashboard4Component {
+
+}
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,12 +1,22 @@
+import { Dashboard4Component } from './admin dashboard/dashboard4/dashboard4.component';
+import { Dashboard3Component } from './admin dashboard/dashboard3/dashboard3.component';
+import { Dashboard1Component } from './admin dashboard/dashboard1/dashboard1.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { ModalsModule, WidgetsModule } from '../../_metronic/partials';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Dashboard2Component } from './admin dashboard/dashboard2/dashboard2.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    Dashboard1Component,
+    Dashboard2Component,
+    Dashboard3Component,
+    Dashboard4Component,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([
@@ -19,5 +29,11 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
     ModalsModule,
     NgbNavModule,
   ],
+  exports: [
+    Dashboard1Component,
+    Dashboard2Component,
+    Dashboard3Component,
+    Dashboard4Component
+  ]
 })
-export class DashboardModule {}
+export class DashboardModule { }
